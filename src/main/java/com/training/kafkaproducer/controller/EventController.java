4ca253bf7557eff62f5b4bc3e222ae5b3b9855df
Boga <Kalyan.Boga@zimmerbiomet.com>
2024-09-05 09:10:03 -0400

    first commit

---
+++ src/main/java/com/training/kafkaproducer/controller/EventController.java
@@ -0,0 +1,31 @@
+package com.training.kafkaproducer.controller;
+
+import com.training.kafkaproducer.service.KafkaMessagePublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producer-app")
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher publisher;
+
+    @GetMapping("/topic/{topic}/message/{message}")
+    public ResponseEntity<?> publishMessage(@PathVariable String topic,@PathVariable String message){
+        try {
+            for(int i =0; i<10000; i++){
+                publisher.sendMessageToTopic(topic, message+":"+i);
+            }
+
+            return ResponseEntity.ok("Message Published Successfully ...");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
